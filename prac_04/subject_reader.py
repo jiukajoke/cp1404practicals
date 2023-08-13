5fb5634ff4967adf6ba1edf7590a0e0899a02c1d
joke03 <93513471+joke03@users.noreply.github.com>
2023-08-13 23:07:35 +0800

    Reformat this file so the dictionary code follows PEP 8 convention

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -2,28 +2,35 @@
 CP1404/CP5632 Practical
 Data file -> lists program
 """
-
 FILENAME = "subject_data.txt"
 
 
 def main():
     data = get_data()
-    print(data)
+    display_subject_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        parts[2] = int(parts[2])  # Make the number an integer
+        subject_data.append(parts)  # Add the processed data to the list
     input_file.close()
+    return subject_data
 
 
-main()
+def display_subject_details(subject_data):
+    """Display subject details in the desired format."""
+    for subject_info in subject_data:
+        subject = subject_info[0]
+        lecturer = subject_info[1]
+        num_students = subject_info[2]
+        print(f"{subject} is taught by {lecturer} and has {num_students} students")
+
+
+if __name__ == "__main__":
+    main()
