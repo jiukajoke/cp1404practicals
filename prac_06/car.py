c0142dfc6f1b253c3504c1a4905843a0fa441b79
joke03 <93513471+joke03@users.noreply.github.com>
2023-08-28 00:04:21 +0800

    Added a name field to the Car class

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,11 +4,13 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name, fuel=0):
         """Initialise a Car instance.
 
+        name: str, the name of the car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
@@ -31,8 +33,9 @@ class Car:
         return distance
 
     def __str__(self):
-        """Return a string representation of the Car."""
-        return f"Car, fuel={self.fuel}, odometer={self._odometer}"
+        """Return a string representation of the car."""
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
+
 
 my_car = Car(fuel=200)
 print(my_car)
